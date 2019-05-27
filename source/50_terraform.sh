b90af077efbad6108c25ff03ba958defe7b1c762
Chris Bailey <cbailey@benevity.com>
2019-05-27 16:11:13 -0600

    Updating terraform path to use v11 not v12

--- source/50_terraform.sh
+++ source/50_terraform.sh
@@ -1,3 +1,5 @@
 alias tfplan="terraform plan -out ~/ops/tfplans/\$(basename \$(pwd))_\$(date +%F_%H_%M_%S).tfplan"
 alias tfapply="terraform apply ~/ops/tfplans/\$(ls -w1 -t ~/ops/tfplans | head -1)"
 alias tfinit="terraform init"
+
+export PATH="/usr/local/opt/terraform@0.11/bin:$PATH"
